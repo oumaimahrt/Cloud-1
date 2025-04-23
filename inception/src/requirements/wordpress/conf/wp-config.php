ca21153a8c41362241f3b83aff2bcfb3ee82df20
NouhailaElBrighli <nouhailaelbrighli1337@gmail.com>
2025-04-23 15:18:41 +0100

    generate certs

--- inception/src/requirements/wordpress/conf/wp-config.php
+++ inception/src/requirements/wordpress/conf/wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'wordpress_database' );
 define( 'DB_USER', 'kait-mar' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'login123' );
+define( 'DB_PASSWORD', '${DB_PASSWORD}' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'mariadb' );
